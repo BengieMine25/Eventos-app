48335623627d474714a6c4b9d18d969fec3b559a
BengieMine25 <est.bmedinao0201@uml.edu.ni>
2023-04-21 18:29:22 -0500

    Actualizacion

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+//Cargar los estilos de la aplicacion
+import '/src/css/style.css'
+
+//Activar el plugin de flowbite
+import 'flowbite'
+
+//Cargar la barra de navegacion
+import './barra'
+
+//Cargar el carrusel/slider
+import './slider'
+
+//Cargar header
+import './header'
